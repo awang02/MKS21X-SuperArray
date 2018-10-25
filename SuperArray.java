088b4d2e6368b4de01f5b2c10607a36760356524
Allen Wang <awang02@stuy.edu>
2018-10-25 14:45:43 -0400

    Worked on Phase 1

---
+++ SuperArray.java
@@ -0,0 +1,24 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+
+  public SuperArray () {
+    String[] data = new String[10];
+  }
+
+  public static void clear() {
+    String[] data = {};
+  }
+
+  public int size() {
+    return data.length;
+  }
+
+  public boolean isEmpty() {
+    if (this.size == 0) {
+      return true;
+    }
+  }
+
+  
+}
